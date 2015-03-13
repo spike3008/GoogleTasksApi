44896baf6e993dc9f6b0d36de8b17c6ca630d457
spike3008 <spike3008@gmail.com>
2015-03-13 10:57:44 +0100

    AccountPicker added

--- app/src/main/java/com/android/madd/googletasksapi/LogInFragment.java
+++ app/src/main/java/com/android/madd/googletasksapi/LogInFragment.java
@@ -1,17 +1,25 @@
 package com.android.madd.googletasksapi;
 
+import android.accounts.AccountManager;
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.AccountPicker;
+
 import butterknife.ButterKnife;
 
 import static timber.log.Timber.i;
 
 public class LogInFragment extends Fragment {
+
+    private static final int PICK_ACCOUNT_REQUEST = 1;
 
     public LogInFragment() {
     }
@@ -28,6 +36,18 @@ public class LogInFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         i("LogInFragment created.");
+
+        Intent googlePicker = AccountPicker.newChooseAccountIntent(null, null,
+                new String[]{GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE}, true, null, null, null, null);
+        startActivityForResult(googlePicker, PICK_ACCOUNT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_ACCOUNT_REQUEST && resultCode == Activity.RESULT_OK) {
+            String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+            i("Account Name= " + accountName);
+        }
     }
 
     @Override
